9f5eec9ec0a0d3a747a5d068871d0a67e7e7eb76
egosha7 <egor.sharik@yandex.ru>
2023-10-03 16:30:19 +0500

    update

--- api/handlers/user_handler.go
+++ api/handlers/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/jackc/pgx/v4"
+	"go.uber.org/zap"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
@@ -14,17 +15,22 @@ type User struct {
 	Password string `json:"password"`
 }
 
-func RegisterUser(w http.ResponseWriter, r *http.Request, conn *pgx.Conn) {
+func RegisterUser(w http.ResponseWriter, r *http.Request, conn *pgx.Conn, logger *zap.Logger) {
 	// Парсинг JSON-данных из запроса
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
+		logger.Error("Ошибка при разборе JSON", zap.Error(err))
 		http.Error(w, "Ошибка при разборе JSON", http.StatusBadRequest)
 		return
 	}
 
+	// Отладочный лог: вывод данных о полученном пользователе
+	logger.Debug("Получен запрос на регистрацию пользователя", zap.Any("user", user))
+
 	// Проверка наличия обязательных полей в запросе
 	if user.Login == "" || user.Password == "" {
+		logger.Error("Неверный формат запроса: отсутствуют обязательные поля")
 		http.Error(w, "Неверный формат запроса", http.StatusBadRequest)
 		return
 	}
@@ -33,10 +39,12 @@ func RegisterUser(w http.ResponseWriter, r *http.Request, conn *pgx.Conn) {
 	var existingUser string
 	err = conn.QueryRow(r.Context(), "SELECT login FROM users WHERE login = $1", user.Login).Scan(&existingUser)
 	if err != nil && err != pgx.ErrNoRows {
+		logger.Error("Ошибка при выполнении запроса к базе данных", zap.Error(err))
 		http.Error(w, "Ошибка при выполнении запроса к базе данных", http.StatusInternalServerError)
 		return
 	}
 	if existingUser != "" {
+		logger.Info("Пользователь с таким логином уже существует", zap.String("login", user.Login))
 		http.Error(w, "Пользователь с таким логином уже существует", http.StatusConflict)
 		return
 	}
@@ -44,6 +52,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request, conn *pgx.Conn) {
 	// Хэширование пароля перед сохранением
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
+		logger.Error("Ошибка хэширования пароля", zap.Error(err))
 		http.Error(w, "Ошибка хэширования пароля", http.StatusInternalServerError)
 		return
 	}
@@ -51,12 +60,14 @@ func RegisterUser(w http.ResponseWriter, r *http.Request, conn *pgx.Conn) {
 	// Вставка нового пользователя в базу данных
 	_, err = conn.Exec(r.Context(), "INSERT INTO users (login, password) VALUES ($1, $2)", user.Login, hashedPassword)
 	if err != nil {
+		logger.Error("Ошибка при добавлении пользователя в базу данных", zap.Error(err))
 		http.Error(w, "Ошибка при добавлении пользователя в базу данных", http.StatusInternalServerError)
 		return
 	}
 
 	// Ответ клиенту
 	w.WriteHeader(http.StatusCreated)
+	logger.Info("Пользователь успешно зарегистрирован", zap.String("login", user.Login))
 	fmt.Fprintf(w, "Пользователь %s успешно зарегистрирован", user.Login)
 }
 
